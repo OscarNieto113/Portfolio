a81a9882737581c035305551e9883c915b868c63
Oscar Nieto <a01705090@itesm.mx>
2023-08-26 00:05:46 -0600

    Grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,29 @@
-const index = () => {
-    return (
-        <div>
-            <h1>
-                Hola mundo
-            </h1>
+import {
+    NextPage,
+  } from "next";
+
+const About: NextPage = () => {
+  // console.log(services);
+
+  return (
+    <div className="flex flex-col flex-grow px-6 pt-1 ">
+      <h6 className="my-3 text-base font-medium">
+        
+      </h6>
+      <div
+        className="flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100 "
+        style={{ marginLeft: "-1.5rem", marginRight: "-1.5rem" }}
+      >
+        <h4 className="my-3 text-xl font-semibold tracking-wide">
+          
+        </h4>
+
+        <div className="grid gap-6 my-3 md:grid-cols-2">
+            
         </div>
-    )
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default index
+export default About;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["ui-sans-serif"],
+    },
+    boxShadow: {
+      "custom-light": " 0 0 10px #313131",
+      "custom-dark": "5px 5px 10px #0a0c0e , -5px -5px 10px #14161c",
+    },
+
   },
   plugins: [],
-}
-
+};
