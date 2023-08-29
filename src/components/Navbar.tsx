57058aa29a2a54fedb0c58a40b2882307b58c4aa
OscarNieto113 <a01705090@itesm.mx>
2023-08-29 14:53:34 -0600

    Navbar Responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,13 +16,47 @@ const Navbar = () => {
     navItems.find((item) => item.route === pathname)?.name || "";
   const [active, setActive] = useState(initialActive);
 
-  return (
-    <div className="flex items-center justify-between px-5 py-3 my-3">
-      <span className="text-xl font-bold border-b-4 md:text-2xl border-orange">
-        {active}
-      </span>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-      <div className="text-base font-normal md:text-xl">
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      setIsMenuOpen(false);
+    }
+  }, [pathname]);
+
+  return (
+    <div className="flex flex-col justify-between px-5 py-3 bg-gray-300 md:flex-row md:items-center">
+      <div className="flex items-center justify-between w-full md:w-auto">
+        <span className="text-xl font-bold border-b-4 md:text-2xl border-orange">
+          {active}
+        </span>
+        <button
+          className="text-gray-600 md:hidden hover:text-gray-800 focus:outline-none hover:scale-155"
+          onClick={toggleMenu}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </button>
+      </div>
+
+      <div
+        className={`mt-4 md:mt-0 md:flex md:items-center ${
+          isMenuOpen ? "block" : "hidden"
+        }`}
+      >
         {navItems.map(({ name, route }) => (
           <NavbarItem
             key={name}
