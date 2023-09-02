cf039940afe8d54cebf836751e5866c5de42f3b6
Oscar Nieto <a01705090@itesm.mx>
2023-09-01 21:22:31 -0600

    Functionality addons

--- src/components/Project/FilterDropdown.tsx
+++ src/components/Project/FilterDropdown.tsx
@@ -21,11 +21,17 @@ const FilterDropdown: FunctionComponent<FilterDropdownProps> = ({
     onSelectFilter(updatedFilters);
   };
 
+  const clearAllFilters = () => {
+    onSelectFilter([]);
+  };
+
   return (
     <div className="relative inline-block text-left">
       <button
         type="button"
-        className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring focus:ring-blue-500 active:bg-gray-200"
+        className={`inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring focus:ring-blue-500 active:bg-gray-200 ${
+          isDropdownOpen ? "bg-blue-100" : ""
+        }`}
         aria-expanded={isDropdownOpen}
         onClick={() => setIsDropdownOpen(!isDropdownOpen)}
         aria-haspopup="listbox"
@@ -71,6 +77,38 @@ const FilterDropdown: FunctionComponent<FilterDropdownProps> = ({
             </div>
           ))}
         </div>
+      )}
+
+      <div
+        className="flex flex-wrap mb-2 space-x-2"
+        role="listbox" // Add role attribute
+      >
+        {selectedFilters.map((filter) => (
+          <div
+            key={filter}
+            className="px-2 py-1 text-sm font-medium text-gray-700 bg-blue-100 rounded-md cursor-pointer focus:outline-none focus:ring focus:ring-blue-300"
+            onClick={() => handleFilterSelection(filter)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleFilterSelection(filter);
+              }
+            }}
+            tabIndex={0} // Add tabIndex for keyboard accessibility
+          >
+            {filter}
+            <span className="ml-1 text-red-500">×</span>
+          </div>
+        ))}
+      </div>
+
+      {selectedFilters.length > 0 && (
+        <button
+          type="button"
+          className="text-sm text-gray-500 hover:text-red-500 focus:outline-none focus:ring focus:ring-red-300"
+          onClick={clearAllFilters}
+        >
+          Clear All
+        </button>
       )}
     </div>
   );
