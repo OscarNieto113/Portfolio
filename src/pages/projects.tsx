a0a6034c2a8c02ccb93b83adfd1defbfc4df11fe
OscarNieto113 <a01705090@itesm.mx>
2023-09-02 22:29:27 -0600

    Minor Visual Changes

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ProjectCard from "../components/Project/ProjectCard";
-import FilterDropdown from "@/components/Project/FilterDropdown";
 import { projects as projectsData } from "../data/projects";
+import  FilterDropdown  from "@/components/Project/FilterDropdown";
 import { generalSkills, GeneralSkill } from "../data/generalSkills";
 
 // Extract unique category values from generalSkills
@@ -10,47 +10,42 @@ const categories: string[] = Array.from(
 );
 
 const Projects = () => {
+  const [projects, setProjects] = useState(projectsData);
+  const [active, setActive] = useState("all");
   const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
 
   const handleSelectFilter = (selectedFilters: string[]) => {
     setSelectedFilters(selectedFilters);
-  };
 
-  const filterProjects = (selectedFilters: string[]) => {
+    // Apply selected filters to the projects.
     if (selectedFilters.length === 0) {
-      return projectsData;
+      setProjects(projectsData);
+      setActive("all");
     } else {
-      return projectsData.filter((project) =>
+      const filteredProjects = projectsData.filter((project) =>
         selectedFilters.every((filter) => project.category.includes(filter))
       );
+      setProjects(filteredProjects);
+      setActive("filtered");
     }
   };
 
-  const filteredProjects = filterProjects(selectedFilters);
-
   return (
-<div className="grid grid-cols-12 gap-4 mx-3 mt-2">
-  <div className="col-span-12 md:col-span-3 lg:col-span-2">
-    <FilterDropdown
-      categories={categories}
-      selectedFilters={selectedFilters}
-      onSelectFilter={handleSelectFilter}
-    />
-  </div>
+    <div className="px-5 py-2 overflow-y-scroll" style={{ height: "65vh" }}>
+      <FilterDropdown
+        categories={categories}
+        selectedFilters={selectedFilters}
+        onSelectFilter={handleSelectFilter}
+      />
 
-  <div className="relative grid grid-cols-12 col-span-12 gap-4 md:col-span-9">
-    {filteredProjects.map((project) => (
-      <div
-        className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200"
-        key={project.nameProject}
-      >
-        <ProjectCard project={project} />
-        
+      <div className="relative grid grid-cols-12 gap-4 my-3">
+        {projects.map((project) => (
+          <div className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200">
+            <ProjectCard project={project} key={project.nameProject} />
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
-</div>
-
+    </div>
   );
 };
 
